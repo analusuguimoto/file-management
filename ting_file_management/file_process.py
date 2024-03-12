e68b5d4f765898122868afb4f931fbffcdc94b1b
Ana Lourenco <analuizajl5@gmail.com>
2024-03-12 16:06:24 -0300

    req 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,22 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for name in instance.queue:
+        if name["nome_do_arquivo"] == path_file:
+            return name
+
+    lines_info = txt_importer(path_file)
+    if lines_info is not None:
+        dicti = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(lines_info),
+            "linhas_do_arquivo": lines_info,
+        }
+
+    instance.enqueue(dicti)
+    sys.stdout.write(str(dicti))
 
 
 def remove(instance):

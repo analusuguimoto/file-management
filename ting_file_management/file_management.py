85786daddedb9eee10edb4772222021fb05d011d
Ana Lourenco <analuizajl5@gmail.com>
2024-03-12 15:38:40 -0300

    req 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        sys.stderr.write("Formato inválido\n")
+
+    try:
+        with open(path_file, "r") as file:
+            info = file.read().split("\n")
+
+    except FileNotFoundError:
+        return sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+
+    return info

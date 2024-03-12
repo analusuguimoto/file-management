29deca9de3d928dfeac3a698252a32a271dbc756
Ana Lourenco <analuizajl5@gmail.com>
2024-03-12 16:21:46 -0300

    req 5

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -31,4 +31,8 @@ def remove(instance):
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        info = instance.search(position)
+        sys.stdout.write(str(info))
+    except IndexError:
+        sys.stderr.write("Posição inválida")

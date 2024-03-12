9f310724eab443ed7131ae862b40af6067002bf8
Ana Lourenco <analuizajl5@gmail.com>
2024-03-12 17:40:49 -0300

    req 7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,22 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    words_info = []
+    for data in range(len(instance)):
+        ocurrencies = []
+        info = instance.search(data)
+
+        for index, line in enumerate(info["linhas_do_arquivo"], start=1):
+            if word in line.lower():
+                ocurrencies.append({"linha": index})
+
+        if ocurrencies:
+            result = {
+                "palavra": word,
+                "arquivo": info["nome_do_arquivo"],
+                "ocorrencias": ocurrencies,
+            }
+            words_info.append(result)
+
+    return words_info
 
 
 def search_by_word(word, instance):
